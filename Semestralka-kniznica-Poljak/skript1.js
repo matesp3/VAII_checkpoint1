5bfeb8bffb574b72d01e0a8452978436372cce3e
matesp3 <148990870+matesp3@users.noreply.github.com>
2023-10-25 17:09:51 +0200

    Upload verzie 1 checkpoint1

---
+++ Semestralka-kniznica-Poljak/skript1.js
@@ -0,0 +1,18 @@
+// window.addEventListener('mousewheel', function () {
+//     // const navbarHeight = document.getElementsByClassName(".mojNav").width;
+//     // const stickyContainer = document.querySelector(".stickyContainer");
+//     // if (stickyContainer != null) {
+//     //     // stickyContainer.setAttribute("top", "50");
+//     //     stickyContainer.style.top = "500px";
+//     // }
+//     // divElement = document.querySelector(".mojNav");
+//
+//     elemHeight = divElement.offsetHeight;
+//     document.querySelector(".mojNav")
+//     el2 = document.querySelector(".stickyContainer");
+//     // inPixels = "top:" +elemHeight.toString() + "px"
+//     el2.style.backgroundColor = "red";
+//     el2.style.width = "100%"
+//     el2.style.height = "20px"
+//     el2.style.top = "56px";
+// });
